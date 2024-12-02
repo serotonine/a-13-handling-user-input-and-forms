dd67386f0a30019aeb2155324526d89cc59e8d61
serotonine <ks.serotonine@gmail.com>
2024-12-02 14:01:38 +0100

    Reactive form.

--- README.md
+++ README.md
@@ -15,6 +15,9 @@ Build [Maximilian Schwarzmüller](https://www.udemy.com/user/maximilian-schwarzm
 - [Template-driven: Registering Form Controls](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129706)
   You can use NgModel not in two way binding. 
   - `<form #loginForm="ngForm" (ngSubmit)="onSubmit(loginForm)">` => provide an object with value: all the input !!
-- [Validating Input with Form Validation Directives](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129726)
-- [Adding Validation Styles](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129734)
-- [Populate input] (https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129752)
+  - [Validating Input with Form Validation Directives](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129726)
+  - [Adding Validation Styles](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129734)
+  - [Populate input] (https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129752)
+- [ Reactive Forms] (https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129756)
+  The diff with template driven is you don't set up the form with the template.
+  - [ReactiveFormsModule](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129762)
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,61 +1,33 @@
-import {
-  afterNextRender,
-  Component,
-  DestroyRef,
-  inject,
-  viewChild,
-} from '@angular/core';
-import { FormsModule, NgForm, NgModel } from '@angular/forms';
-import { debounceTime } from 'rxjs';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  // Save entered value to get them on reload.
-  // Observable.
-  private _form = viewChild.required<NgForm>('loginForm');
-  private _destroyRef = inject(DestroyRef);
-  constructor() {
-    afterNextRender(() => {
-      const storageData = window.localStorage.getItem('email');
-      if (storageData) {
-        const loadedData = JSON.parse(storageData);
-        const loadedEmail = loadedData.email;
-        /* this._form().setValue({
-          email: loadedEmail,
-          password:''}); */
-        setTimeout(() => {
-          this._form().controls['email'].setValue(loadedEmail);
-        }, 1);
-      }
-      const subscription = this._form()
-        .valueChanges?.pipe(debounceTime(500))
-        .subscribe({
-          next: (value) =>
-            window.localStorage.setItem(
-              'email',
-              JSON.stringify({ email: value.email })
-            ),
-        });
-      this._destroyRef.onDestroy(() => subscription?.unsubscribe());
-    });
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
+  get isInvalidEmail(){
+    return this.form.controls.email.touched && this.form.controls.email.dirty && this.form.controls.email.invalid;
   }
-
-  onChange(pwd: NgModel) {
-    console.log(pwd.value);
+  get isInvalidPwd(){
+    return this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid;
   }
-  onSubmit(formData: NgForm) {
-    console.log('[LoginComponent on submit form]');
-    console.log(formData);
-    const _form = formData.form;
-    const emailInput = formData.form.value.email;
-    const pwdInput = formData.form.value.password;
-    // Lot of disponible functions.
-    _form.reset();
+  onSubmit() {
+    Object.entries(this.form.controls).map((item) =>
+      console.log(item[1].value)
+    );
+    console.log(this.form.value);
+    this.form.reset();
   }
 }
