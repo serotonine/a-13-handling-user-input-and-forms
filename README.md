87588eb974f6fbd86dfe66c49f5489b2d9518be7
serotonine <ks.serotonine@gmail.com>
2024-12-02 18:43:11 +0100

    Creating & Using Async Validators.

--- README.md
+++ README.md
@@ -21,4 +21,5 @@ Build [Maximilian Schwarzmüller](https://www.udemy.com/user/maximilian-schwarzm
 - [ Reactive Forms] (https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129756)
   The diff with template driven is you don't set up the form with the template.
   - [ReactiveFormsModule](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129762)
-- [Building Custom Validators](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129776)
+  - [Building Custom Validators](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129776)
+  - [Creating & Using Async Validators](https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44129778)
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { of } from 'rxjs';
 
 // Custom validator.
-function hasNumber(control: AbstractControl){
+function hasNumber(control: AbstractControl) {
   const hasN = /[0-9]+/.test(control.value);
   console.log(hasN);
-  return hasN ? null: {valid:true};
+  return hasN ? null : { valid: true };
+}
+// Custom Async Validator.
+function isUniqueEmail(control: AbstractControl) {
+  if (control.value !== 'test@example.com') {
+    return of(null);
+  }
+  return of({ dupplicateEmail: true });
 }
 
 @Component({
@@ -15,21 +29,29 @@ function hasNumber(control: AbstractControl){
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-
 export class LoginComponent {
   form = new FormGroup({
     email: new FormControl('', {
       validators: [Validators.required, Validators.email],
+      asyncValidators: [isUniqueEmail],
     }),
     password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(6), hasNumber],
     }),
   });
-  get isInvalidEmail(){
-    return this.form.controls.email.touched && this.form.controls.email.dirty && this.form.controls.email.invalid;
+  get isInvalidEmail() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
   }
-  get isInvalidPwd(){
-    return this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid;
+  get isInvalidPwd() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
   }
   onSubmit() {
     Object.entries(this.form.controls).map((item) =>
